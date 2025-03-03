bfd44709dd6527fed3bc378d193d154c51f6d9f7
sadiiq-mukhtaar <sadiiq2077@gmail.com>
2025-03-03 03:33:48 -0800

    building api for creating an issue

---
+++ app/api/issue/create/route.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+interface Body {
+  title: string;
+  description: string;
+}
+
+const createIsssueSchema = z.object({
+  title: z.string().min(1).max(250),
+  description: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  const body: Body = await req.json();
+  const validation = createIsssueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
