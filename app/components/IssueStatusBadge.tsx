4817fe6f347ff7172fc00ab20730ccb1de2bf542
sadiiq-mukhtaar <sadiiq2077@gmail.com>
2025-03-05 20:24:42 -0800

    build : issue status badge component

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,11 @@
+import { Status } from "@prisma/client";
+import { Badge } from "@radix-ui/themes";
+import React from "react";
+
+const IssueStatusBadge = ({ status }: { status: Status }) => {
+  if (status === "OPEN") return <Badge color="red">{status}</Badge>;
+  if (status === "IN_PROGRESS") return <Badge color="violet">{status}</Badge>;
+  if (status === "CLOSED") return <Badge color="green">{status}</Badge>;
+};
+
+export default IssueStatusBadge;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,45 @@
-import { Button } from "@radix-ui/themes";
+import { prisma } from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import IssueStatusBadge from "../components/IssueStatusBadge";
 
-const IssuePage = () => {
+const IssuePage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">Add New Issue</Link>
-      </Button>
+      <div className="mb-4">
+        <Button>
+          <Link href="/issues/new">Add New Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.Cell>Issue</Table.Cell>
+            <Table.Cell className="hidden md:table-cell">Status</Table.Cell>
+            <Table.Cell className="hidden md:table-cell">Created</Table.Cell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="mt-1 md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
