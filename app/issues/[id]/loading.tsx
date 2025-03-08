7cd1d8cb02d1a4c911eeb1df2ec0d509f59376ed
sadiiq-mukhtaar <sadiiq2077@gmail.com>
2025-03-08 06:55:36 -0800

    add : additional skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const loadingIssueDetail = () => {
-  return <div>loading...</div>;
+  return (
+    <div className="max-w-[30rem]">
+      <>
+        <Skeleton width={"6rem"} height={"2rem"} />
+        <div className="mt-8">
+          <Skeleton height={"1.2rem"} />
+          <Flex gap={"10px"} className="items-center" my={"3"}>
+            <Skeleton width={"4rem"} height={"1.2rem"} />
+            <Skeleton width={"5rem"} height={"1.2rem"} />
+          </Flex>
+          <Skeleton count={5} height={"1.2rem"} className="mt-2" />
+        </div>
+      </>
+    </div>
+  );
 };
 
 export default loadingIssueDetail;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,15 @@
+import { Box } from "@radix-ui/themes";
 import React from "react";
-
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 const loadingNewIssue = () => {
-  return <div>loading...</div>;
+  return (
+    <Box className="mx-x-[30rem]">
+      <p>loading...</p>
+      <Skeleton />
+      <Skeleton height={"18rem"} />
+    </Box>
+  );
 };
 
 export default loadingNewIssue;
