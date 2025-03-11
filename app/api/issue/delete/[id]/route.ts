ed7a9f6ce6ff08393ca141720e860ed6ddd7870c
sadiiq-mukhtaar <sadiiq2077@gmail.com>
2025-03-11 09:58:09 -0700

    build : api for deleting issues

---
+++ app/api/issue/delete/[id]/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/prisma/client";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json({});
+}
