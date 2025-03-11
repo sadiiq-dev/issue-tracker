3af6434db31ee4894fd3fb7d6f1fad73f7a426d0
sadiiq-mukhtaar <sadiiq2077@gmail.com>
2025-03-11 09:36:55 -0700

    add : alert dialog

--- app/issues/[id]/DeleteButton.tsx
+++ app/issues/[id]/DeleteButton.tsx
@@ -1,16 +1,37 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 import { AiFillDelete } from "react-icons/ai";
 
 const DeleteButton = ({ issueId }: { issueId: Number }) => {
   return (
-    <div>
-      <Button color="red">
-        <AiFillDelete />
-        Edit The Issue
-      </Button>
-    </div>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <div>
+          <Button color="red">
+            <AiFillDelete />
+            Edit The Issue
+          </Button>
+        </div>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm To Delete</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue ? make sure this action
+          don't have undo{" "}
+        </AlertDialog.Description>
+        <Flex mt={"4"} gap={"6"}>
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
